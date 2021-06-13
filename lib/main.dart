e58cdedf21135eadb0c6c4f911cc1cac70dedab2
Kush Agarwal <36kushagarwal@gmail.com>
2021-06-13 16:03:15 +0530

    "Version 1"

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/note_list.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KAD ToDo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
+  }
+}
+
